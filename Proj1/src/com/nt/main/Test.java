c2d60e9c3e0a9342c929f172a7e14cddaac9b1fa
My pc <My pc@DESKTOP-0GR9NN5>
2023-09-30 14:43:00 +0530

    TL-create main file for result

---
+++ Proj1/src/com/nt/main/Test.java
@@ -0,0 +1,13 @@
+package com.nt.main;
+
+import com.nt.dao.Xyz;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Xyz xyz=new Xyz();
+		int result=xyz.add(10, 20);
+		System.out.println("Result ::"+result);
+	}
+
+}
