f3bc1dc70a280f3c44bd4a5e1b7ef552d03a5e29
My pc <My pc@DESKTOP-0GR9NN5>
2023-10-03 23:44:15 +0530

    change in B2 commit

---
+++ Proj1/src/com/nt/dao/C3.java
@@ -0,0 +1,8 @@
+package com.nt.dao;
+
+public class C3 {
+	public void calcSimpleInterestAmount(float p,float t,float r) {
+		float csia=p*r*t/100;
+		System.out.println("Simple Interest ::"+csia);
+	}
+}
